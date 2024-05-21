b0b6b2731723a32c850787870bb583fcfa5f4d5c
lamha <la210006@stuent.helha.be>
2024-05-21 15:03:47 +0200

    Interface pour les armes

--- Groupe05/src/main/java/be/helha/dao/Dao.java
+++ Groupe05/src/main/java/be/helha/dao/Dao.java
@@ -1,5 +1,14 @@
 package be.helha.dao;
 
-public interface Dao {
+import java.util.List;
 
+/**
+ * Interface générique pour les opérations CRUD.
+ */
+public interface Dao<Arme> {
+    void ajouter(Arme t);
+    List<Arme> obtenirTous();
+    void mettreAJour(Arme t);
+    void supprimer(int id);
 }
+
