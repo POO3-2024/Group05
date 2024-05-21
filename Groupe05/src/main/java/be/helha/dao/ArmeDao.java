c75abdc2d4a7256b46c21cb70b0f19c2ee045c69
lamha <la210006@stuent.helha.be>
2024-05-21 20:58:09 +0200

    dev de ArmeDao et ArmeDaoImpl

--- Groupe05/src/main/java/be/helha/dao/ArmeDao.java
+++ Groupe05/src/main/java/be/helha/dao/ArmeDao.java
@@ -4,9 +4,9 @@ import be.helha.domaine.Arme;
 import java.util.List;
 
 public interface ArmeDao {
-    void ajouterArme(Arme arme);
-    Arme obtenirArmeParId(int id);
     List<Arme> obtenirToutesLesArmes();
-    void mettreAJourArme(Arme arme);
+    void ajouterArme(Arme arme);
     void supprimerArme(int id);
+    void mettreAJourArme(Arme arme);
+    Arme obtenirArmeParId(int id);
 }
--- Groupe05/src/main/java/be/helha/daoimpl/ArmeDaoImpl.java
+++ Groupe05/src/main/java/be/helha/daoimpl/ArmeDaoImpl.java
@@ -3,57 +3,32 @@ package be.helha.daoimpl;
 import be.helha.dao.ArmeDao;
 import be.helha.domaine.Arme;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ArmeDaoImpl implements ArmeDao {
-    private Connection connection;
+
+    private final Connection connection;
 
     public ArmeDaoImpl(Connection connection) {
         this.connection = connection;
     }
 
     @Override
-    public void ajouterArme(Arme arme) {
-        String sql = "INSERT INTO armes (nom, degats) VALUES (?, ?)";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            pstmt.setString(1, arme.getNom());
-            pstmt.setInt(2, arme.getDegats());
-            pstmt.executeUpdate();
-
-            ResultSet generatedKeys = pstmt.getGeneratedKeys();
-            if (generatedKeys.next()) {
-                arme.setId(generatedKeys.getInt(1));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public Arme obtenirArmeParId(int id) {
-        String sql = "SELECT * FROM armes WHERE id = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setInt(1, id);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return new Arme(rs.getInt("id"), rs.getString("nom"), rs.getInt("degats"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
     public List<Arme> obtenirToutesLesArmes() {
         List<Arme> armes = new ArrayList<>();
         String sql = "SELECT * FROM armes";
-        try (Statement stmt = connection.createStatement()) {
-            ResultSet rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-                armes.add(new Arme(rs.getInt("id"), rs.getString("nom"), rs.getInt("degats")));
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String nom = resultSet.getString("nom");
+                int degats = resultSet.getInt("degats");
+                armes.add(new Arme(id, nom, degats));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -62,13 +37,12 @@ public class ArmeDaoImpl implements ArmeDao {
     }
 
     @Override
-    public void mettreAJourArme(Arme arme) {
-        String sql = "UPDATE armes SET nom = ?, degats = ? WHERE id = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, arme.getNom());
-            pstmt.setInt(2, arme.getDegats());
-            pstmt.setInt(3, arme.getId());
-            pstmt.executeUpdate();
+    public void ajouterArme(Arme arme) {
+        String sql = "INSERT INTO armes (nom, degats) VALUES (?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, arme.getNom());
+            statement.setInt(2, arme.getDegats());
+            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -77,11 +51,42 @@ public class ArmeDaoImpl implements ArmeDao {
     @Override
     public void supprimerArme(int id) {
         String sql = "DELETE FROM armes WHERE id = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setInt(1, id);
-            pstmt.executeUpdate();
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void mettreAJourArme(Arme arme) {
+        String sql = "UPDATE armes SET nom = ?, degats = ? WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, arme.getNom());
+            statement.setInt(2, arme.getDegats());
+            statement.setInt(3, arme.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Arme obtenirArmeParId(int id) {
+        Arme arme = null;
+        String sql = "SELECT * FROM armes WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String nom = resultSet.getString("nom");
+                int degats = resultSet.getInt("degats");
+                arme = new Arme(id, nom, degats);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return arme;
     }
 }
