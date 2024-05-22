b3db239dc1dec7cd29fb2a38ed1c98bd10cc5589
lamha <la210006@stuent.helha.be>
2024-05-22 03:12:13 +0200

    MonApplication.java MAJ

--- Groupe05/src/main/java/be/helha/application/MonApplication.java
+++ Groupe05/src/main/java/be/helha/application/MonApplication.java
@@ -1,10 +1,16 @@
 package be.helha.application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
 
 
 public class MonApplication extends Application {
@@ -20,8 +26,34 @@ public class MonApplication extends Application {
 			e.printStackTrace();
 		}
 	}
+	@FXML
+	private Button btnMainArmes;
+
+	@FXML
+	private Button btnMainPersos;
+
+	@FXML
+	private Label lblMainMenu;
+
+	@FXML
+	private void goToListeArmes(ActionEvent event) throws IOException {
+		System.out.println("goToListeArmes called");
+		Parent root = FXMLLoader.load(getClass().getResource("/ListeArmes.fxml"));
+		Scene scene = new Scene(root);
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+	}
+
+
+	@FXML
+	void goToListePersos(ActionEvent event) {
+
+	}
 
 	public static void lancer(String[] args) {
 		MonApplication.launch(args);
 	}
+
 }
+
