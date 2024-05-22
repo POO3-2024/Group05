1206dd99323a3a71f20c6b8aa9d5362ff0fd28bc
lamha <la210006@stuent.helha.be>
2024-05-22 03:11:03 +0200

    DaoFactory.java fini

--- Groupe05/src/main/java/be/helha/daoimpl/DaoFactory.java
+++ Groupe05/src/main/java/be/helha/daoimpl/DaoFactory.java
@@ -1,23 +1,42 @@
 package be.helha.daoimpl;
 
 import be.helha.dao.ArmeDao;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Classe fabrique pour obtenir les instances DAO.
+ */
 public class DaoFactory {
-    private static final String URL = "jdbc:sqlite:/c:/sqlite/db/poo3.db";
+    private static ArmeDao armeDaoInstance;
 
-    public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
+    /**
+     * Retourne une instance de ArmeDao.
+     *
+     * @return une instance de ArmeDao
+     */
+    public static ArmeDao getArmeDao() {
+        if (armeDaoInstance == null) {
+            armeDaoInstance = new ArmeDaoImpl(getConnection());
         }
+        return armeDaoInstance;
     }
 
-    public static ArmeDao getArmeDao() {
-        return new ArmeDaoImpl(getConnection());
+    /**
+     * Établit une connexion à la base de données SQLite.
+     *
+     * @return une connexion à la base de données
+     */
+    public static Connection getConnection() {
+        String url = "jdbc:sqlite:C:\\sqlite\\db\\poo3.db";
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
