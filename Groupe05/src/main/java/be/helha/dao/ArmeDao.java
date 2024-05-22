ce59a2bf761680445506bd0287bb4792bbb670c0
lamha <la210006@stuent.helha.be>
2024-05-22 03:09:05 +0200

    ArmeDao.java fini

--- Groupe05/src/main/java/be/helha/dao/ArmeDao.java
+++ Groupe05/src/main/java/be/helha/dao/ArmeDao.java
@@ -1,12 +1,20 @@
 package be.helha.dao;
 
 import be.helha.domaine.Arme;
+
 import java.util.List;
 
+/**
+ * Interface pour les opérations CRUD sur les objets Arme.
+ */
 public interface ArmeDao {
     List<Arme> obtenirToutesLesArmes();
-    void ajouterArme(Arme arme);
+
+    boolean ajouterArme(Arme arme);
+
     void supprimerArme(int id);
+
     void mettreAJourArme(Arme arme);
+
     Arme obtenirArmeParId(int id);
 }
